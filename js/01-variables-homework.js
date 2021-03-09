d4b799eb91df96432f4775923a4239ff7c61e43c
Jolanta Krisciukaite <79446323+JolantaKrisciukaite@users.noreply.github.com>
2021-03-09 22:44:17 +0200

    variables homework

---
+++ js/01-variables-homework.js
@@ -0,0 +1,101 @@
+/* 
+JavaScript mini užduotys
+
+Kintamųjų inicijavimas
+*/
+
+/*1*/
+
+const pirmasSkaicius = 20;
+console.log(pirmasSkaicius);
+
+const antrasSkaicius = 33;
+console.log(antrasSkaicius);
+
+const treciasSkaicius = 18;
+console.log(treciasSkaicius);
+
+console.log('----------')
+
+/*2*/
+
+const pasisveikinimas = 'Sveiki,'
+console.log(pasisveikinimas);
+
+const kreipinys = 'mažučiai.';
+console.log(kreipinys);
+
+const klausimas = 'Ką sapnavote?';
+console.log(klausimas);
+
+console.log('----------')
+
+/*3*/
+
+const sarasas1 = [2, 3, 5, 6, 7];
+console.log(sarasas1);
+
+const sarasas2 = [8, 2, 4, 6, 9];
+console.log(sarasas2);
+
+const sarasas3 = [2, 3, 4, 8, 7];
+console.log(sarasas3);
+
+console.log('----------')
+
+/*4*/
+
+const abc1 = ['l', 'i', 'n', 'a', 's'];
+console.log(abc1);
+
+const abc2 = ['v', 'y', 'n', 'a', 's'];
+console.log(abc2);
+
+const abc3 = ['p', 'i', 'r', 'm', 'a'];
+console.log(abc3);
+
+console.log('----------')
+
+/* Veiksmai su kintamaisiais
+*/
+
+/*1*/
+
+const suma = pirmasSkaicius + antrasSkaicius + treciasSkaicius;
+console.log(suma);
+
+console.log('----------')
+
+/*2*/
+
+const tekstas = pasisveikinimas+' '+kreipinys+' '+klausimas
+console.log(tekstas);
+
+console.log('----------')
+ 
+/*3*/
+
+const verte1 = sarasas1[0] - sarasas1[1] + sarasas1[2] - sarasas1[3] + sarasas1[4];
+console.log(verte1);
+
+const verte2 = sarasas2[0] - sarasas2[1] + sarasas2[2] - sarasas2[3] + sarasas2[4];
+console.log(verte2);
+
+const verte3 = sarasas3[0] - sarasas3[1] + sarasas3[2] - sarasas3[3] + sarasas3[4];
+console.log(verte3);
+
+console.log('----------')
+
+/*4*/ 
+
+const raidziuJunginys1 = abc1[0] + abc1[1] + abc1[2] + abc1[3] + abc1[4] 
+const raidziuJunginys2 = abc2[0] + abc2[1] + abc2[2] + abc2[3] + abc2[4]
+const raidziuJunginys3 = abc3[0] + abc3[1] + abc3[2] + abc3[3] + abc3[4]
+
+
+const sarasuJunginys = raidziuJunginys3 +' , ' + raidziuJunginys2 +' , '+ raidziuJunginys1;
+console.log(sarasuJunginys);
+
+
+
+
