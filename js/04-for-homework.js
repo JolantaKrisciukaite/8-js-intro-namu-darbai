ede637a0857e62816bf88d1c389491944ee953d2
Jolanta Krisciukaite <79446323+JolantaKrisciukaite@users.noreply.github.com>
2021-03-15 22:59:47 +0200

    for-homework-1

---
+++ js/04-for-homework.js
@@ -0,0 +1,79 @@
+/* 
+
+Ciklo For panaudojimas
+
+*/
+
+// 1
+
+// a
+
+for (let i = 0; i <= 0; i++) {
+    console.log('0 iki 0');
+}
+
+console.log('------------');
+
+// b
+
+for (let i = 0; i <= 4; i++) {
+    console.log('0 iki 4');
+}
+
+console.log('------------');
+
+// c
+
+for (let i = 0; i <= 100; i++) {
+    console.log('0 iki 100');
+}
+
+console.log('------------');
+
+// d
+
+for (let i = 574; i <= 815; i++) {
+    console.log('574 iki 815');
+}
+
+console.log('------------');
+
+// e
+
+for (let i = -50; i <= 50; i++) {
+    console.log('-50 iki 50');
+}
+
+console.log('------------');
+
+// f
+
+for (let i = -70; i <= -30; i++) {
+    console.log('-70 iki -30');
+}
+
+console.log('------------');
+
+// 2
+
+// a
+
+console.log('------------');
+
+// 3
+
+// a
+
+console.log('------------');
+
+// b
+
+console.log('------------');
+
+// c
+
+console.log('------------');
+
+// d
+
+
