56664c95f17c5675983271eb88ebb51e2a154a69
Jolanta Krisciukaite <79446323+JolantaKrisciukaite@users.noreply.github.com>
2021-03-16 23:35:19 +0200

    function

---
+++ js/05-function-homework.js
@@ -0,0 +1,26 @@
+/* 
+
+Funkcijos
+
+*/
+
+// 1
+
+// a // b // c // d // i // ii
+
+function tusciaFunkcija() {
+    return false;
+}
+
+console.log(tusciaFunkcija());
+
+// 2 
+
+function daugyba(a, b) {
+    const rez = a * b;
+    return rez;
+}
+
+console.log(daugyba (4, 5));
+
+
