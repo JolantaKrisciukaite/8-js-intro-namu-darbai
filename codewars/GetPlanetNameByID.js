98884477e672620392cf6d5996f6f3447f2ecc17
Jolanta Krisciukaite <79446323+JolantaKrisciukaite@users.noreply.github.com>
2021-03-17 21:08:11 +0200

    Folder

--- codewars/GetPlanetNameByID.js
+++ codewars/GetPlanetNameByID.js
@@ -12,21 +12,29 @@ function getPlanetName(id) {
     switch(id) {
 
       case 1:
-        name = 'Mercury'; break;
+        name = 'Mercury'; 
+        break;
       case 2:
-        name = 'Venus'; break;
+        name = 'Venus'; 
+        break;
       case 3:
-        name = 'Earth'; break;
+        name = 'Earth'; 
+        break;
       case 4:
-        name = 'Mars'; break;
+        name = 'Mars'; 
+        break;
       case 5:
-        name = 'Jupiter'; break;
+        name = 'Jupiter'; 
+        break;
       case 6:
-        name = 'Saturn'; break;
+        name = 'Saturn'; 
+        break;
       case 7:
-        name = 'Uranus'; break;
+        name = 'Uranus'; 
+        break;
       case 8:
-        name = 'Neptune'; break;
+        name = 'Neptune'; 
+        break;
     }
     
     return name;
