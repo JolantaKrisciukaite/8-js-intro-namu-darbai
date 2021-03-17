99c2142464b254f4f3d32bf4beb3c1f1080805e2
Jolanta Krisciukaite <79446323+JolantaKrisciukaite@users.noreply.github.com>
2021-03-17 20:01:07 +0200

    SumOfPositive

---
+++ codewars/SumOfPositive.js
@@ -0,0 +1,35 @@
+/*
+https://www.codewars.com/kata/5715eaedb436cf5606000381/train/javascript
+*/
+
+
+// Jeigu nera ka susumuoti atsakymas bus 0.
+// Panaudoti for'a. 
+// Gauti nurodyta atsakyma
+
+
+// console.log(positiveSum([1,2,3,4,5]), '-->', 15);
+// console.log(positiveSum([1,-2,3,4,5]), '-->', 13);
+// console.log(positiveSum([]), '-->', 0);
+// console.log(positiveSum([-1,-2,-3,-4,-5]), '-->', 0);
+// console.log(positiveSum([-1,2,3,4,-5]), '-->', 9);
+
+
+function positiveSum(array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > 0) {
+            sum += array[i];   
+        }
+    }
+    return sum;
+    
+}
+
+/* 
+
+    function positiveSum(array) {
+    return array.filter(x => x >= 0).reduce((a, b) => a + b, 0);
+  }
+
+*/
