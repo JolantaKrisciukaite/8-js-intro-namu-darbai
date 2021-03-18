e99e99cb016b36c83acdfb389a3a7c7d9f79034a
Jolanta Krisciukaite <79446323+JolantaKrisciukaite@users.noreply.github.com>
2021-03-18 16:01:31 +0200

    Polish Alphabet

--- codewars/Polisch-Alphabet.js
+++ codewars/Polisch-Alphabet.js
@@ -31,3 +31,46 @@ console.log(correctPolishLetters
 
 console.log(correctPolishLetters
 ('Maria Skłodowska-Curie'), '->', 'Maria Sklodowska-Curie');
+
+
+/* 
+
+function correctPolishLetters (string) {
+  var stringArr = string.split("")
+  for (var i = 0; i<stringArr.length; i++){
+    switch (stringArr[i]){
+        case "ą":
+        stringArr[i] = "a";
+        break;
+        case "ć":
+        stringArr[i] = "c";
+        break;
+        case "ę":
+        stringArr[i] = "e";
+        break;
+        case "ł":
+        stringArr[i] = "l";
+        break;
+        case "ń":
+        stringArr[i] = "n";
+        break;
+        case "ó":
+        stringArr[i] = "o";
+        break;
+        case "ś":
+        stringArr[i] = "s";
+        break;
+        case "ź":
+        stringArr[i] = "z";
+        break;
+        case "ż":
+        stringArr[i] = "z";
+        break;
+        default:
+        stringArr[i];
+    }
+  }
+  return stringArr.join("");
+}
+
+*/
