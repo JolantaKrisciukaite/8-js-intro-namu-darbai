9b466d16786170550a59d352c3e314d875fc8dda
Jolanta Krisciukaite <79446323+JolantaKrisciukaite@users.noreply.github.com>
2021-03-20 18:01:02 +0200

    Skaitmenu kiekis skaiciuje

---
+++ js/06-functions-skaitmenu-kiekis-skaiciuje.js
@@ -0,0 +1,25 @@
+
+
+function digits(skaicius) {
+    if (typeof skaicius !== 'number') {
+        return 'ERROR: pateikta netinkamo tipo reiksme';
+    }
+    if (isNaN(skaicius)) {   
+        // arba if ('' + skaicius === 'NaN') {
+        return 'ERROR: pateikta netinkamo tipo reiksme';
+    }
+
+    const tekstinisSkaicius = '' + skaicius;
+    const skaitmenuKiekis = tekstinisSkaicius.length;
+    
+    return skaitmenuKiekis;
+}
+
+
+console.log(digits(5), '-->', 1 );
+console.log(digits(781), '-->', 3);
+console.log(digits(37060123456), '-->', 11);
+console.log(digits(true));
+console.log(digits('asd'));
+console.log(digits(NaN));
+
