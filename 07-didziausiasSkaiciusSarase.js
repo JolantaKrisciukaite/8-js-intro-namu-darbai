6fea540142bf14793f981ca3c4ab2a45304e7af1
Jolanta Krisciukaite <79446323+JolantaKrisciukaite@users.noreply.github.com>
2021-03-21 14:52:20 +0200

    Didziausias skaicius sarase

--- 07-didziausiasSkaiciusSarase.js
+++
@@ -1,2 +0,0 @@
-
-// Funkcija pavadinimu “didziausiasSkaiciusSarase”
---
+++ js/07-functions-didziausiasSkaiciusSarase.js
@@ -0,0 +1,48 @@
+
+// Funkcija pavadinimu “didziausiasSkaiciusSarase”
+
+
+function bigNumb(list) {
+    if (!Array.isArray(list)) {
+    return 'ERROR: pateikta netinkamo tipo reiksme';
+}
+
+    if (list.length === 0) {
+        return 'ERROR: pateiktas sarasas negali buti tuscias';
+    }
+
+    let biggest = list[0];                 // arba -Infinity;
+
+    for (let i = 1; i < list.length; i++) {
+        const number = list[i];
+        if (number > biggest) {
+            biggest = number;
+        }
+    }
+
+    return biggest;
+}
+
+
+console.log(bigNumb([1]), '->', 1);
+console.log(bigNumb([1, 2, 3]), '->', 3);
+console.log(bigNumb([-5, 78, 14, 0, 18]), '->', 78);
+console.log(bigNumb([69, 69, 69, 69, 66]), '->', 69);
+console.log(bigNumb([-1, -2, -3, -4, -5, -6, -7, -8]), '->', -1);
+console.log(bigNumb('pomidoras'));
+console.log(bigNumb([]));
+
+
+/* 
+
+function bigNumb(sarasas) {
+    let len = sarasas.length, max = -Infinity;
+    while (len--) {
+      if (Number(sarasas[len]) > max) {
+        max = Number(sarasas[len]);
+      }
+    }
+    return max;
+  };
+
+*/
