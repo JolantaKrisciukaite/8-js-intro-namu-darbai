b895e6db1f77229da33fd50543560f386a63d09d
Jolanta Krisciukaite <79446323+JolantaKrisciukaite@users.noreply.github.com>
2021-03-23 19:04:48 +0200

    Area of perimeter

--- codewars/area-or-perimeter.js
+++ codewars/area-or-perimeter.js
@@ -1,3 +1,22 @@
 /*
 https://www.codewars.com/kata/5704aea738428f4d30000914/train/javascript
 */
+
+// Kvadrato plotas S = a * b
+// Staciakampio perimetras P = 2 * (a + b)
+// Jeigu kvadratas - grazinkite plota.
+// Jeigu staciakampis - grazinkite perimetra.
+
+const areaOrPerimeter = function(l, w) {
+
+    if (l === w) {
+        return l * w;
+    } else if (l !== w) {
+        return (l + w) * 2;
+    }
+    
+}
+  
+
+console.log(areaOrPerimeter(4 , 4), '->', 16);
+console.log(areaOrPerimeter(6 , 10), '->', 32);
