309daa6587cb7b9fe699cb16e4f3e6340c12c21e
Jolanta Krisciukaite <79446323+JolantaKrisciukaite@users.noreply.github.com>
2021-03-27 14:36:15 +0200

    Pasibandymai

--- js/00-start.js
+++ js/00-start.js
@@ -116,4 +116,132 @@ const pomidoroIlgis = pomidoras.length;
 
 console.log(pomidoroIlgis);
 
+console.clear();
+
+const animal = 'SDGGg';
+
+switch (animal) {
+    case 'dog':
+        console.log('Bark bark');
+        break;
+
+    case 'cat':
+        console.log('Meow meow');
+        break;
+
+    default:
+        console.log('Unknow animal');
+        break;
+}
+
+
+if (animal === 'dog') {
+    console.log('Bark bark');
+} if (animal === 'cat') {
+    console.log('Meow meow');
+} else {
+    console.log('Unknow animal');
+}
+    
+console.log('********************');
+
+
+const day = 2;
+
+switch (day) {
+    case 1:
+        console.log('pirm');
+        break;
+    case 2:
+        console.log('ant');
+        break;
+    case 3:
+        console.log('tre');
+        break;
+    case 4:
+        console.log('ket');
+        break;
+    case 5:
+        console.log('penkt');
+        break;
+    case 6:
+        console.log('sest');
+        break;
+    case 7:
+        console.log('sekm');
+        break;
+    default:
+        console.log('tokia diena savaiteje neegzistuoja');
+        break;
+}
+
+
+switch (day) {
+    case 1: case 2: case 3: case 4: case 5:
+        console.log('darbo diena');
+        break;
+
+        case 6: case 7: 
+        console.log('savaitgalio diena');
+        break;
+
+    default:
+        console.log('tokia diena savaiteje neegzistuoja');
+}
+
+
+switch (day) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        console.log('darbo diena');
+        break;
+
+    case 6:
+    case 7:
+        console.log('savaitgalio diena');
+        break;
+
+    default: 
+        console.log('tokia diena savaiteje neegzistuoja');
+        break;
+
+}
+
+
+if (day === 1 || day === 2 || day === 3 || day === 4 || day === 5) {
+    console.log('darbo diena');
+} else if (day === 6 || day === 7) {
+    console.log('savaitgalio diena');
+} else {
+    console.log('tokia diena savaiteje neegzistuoja');
+}
+
+
+switch (day) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+        console.log('nelygine diena');
+        break;
+
+    case 2:
+    case 6:
+        console.log('lygine diena');
+        break;
+
+    default: 
+        console.log('tokia diena savaiteje neegzistuoja');
+        break;
+}
+
+
+console.log('********************');
+
+
+
+
 
